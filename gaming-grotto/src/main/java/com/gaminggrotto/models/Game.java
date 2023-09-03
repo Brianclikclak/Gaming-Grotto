36a81bddce70e8ba4625e07a396985ee057597ab
Brianclikclak <brianclikclak@hotmail.com>
2023-09-03 13:42:04 +0200

    attributes and relation between entities

--- gaming-grotto/src/main/java/com/gaminggrotto/models/Game.java
+++ gaming-grotto/src/main/java/com/gaminggrotto/models/Game.java
@@ -1,5 +1,55 @@
 package com.gaminggrotto.models;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "games")
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 45)
+    private String title;
+
+    @Column(nullable = true)
+    private Integer releaseYear;
+
+    @Column(nullable = true)
+    private String description;
+
+    @Column(nullable = true)
+    private String image;
+
+    @ManyToOne
+    private Developer developer;
+
+    @ManyToMany
+    @JoinTable(name = "game_platform", joinColumns = @JoinColumn(name = "game_id"), inverseJoinColumns = @JoinColumn(name = "platform_id"))
+
+    private List<Platform> platform;
+
+    @ManyToOne
+    private Genre genre;
 
 }
