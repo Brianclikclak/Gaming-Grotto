36fb77314587b4877e5e7fd567311f195a15085b
Brianclikclak <brianclikclak@hotmail.com>
2023-09-12 12:37:10 +0200

    delete vue files

---
+++ gaming-grotto/src/main/frontend/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+      <img alt="Vue logo" class="logo" src="../assets/images/Grotto-gaming-logo.svg" />
+      <ul>
+          <li>Explorar</li>
+          <li>Generos</li>
+          <li>Plaformas</li>
+          <li>Favoritos</li>
+      </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
