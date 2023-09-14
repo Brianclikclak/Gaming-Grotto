fb5b9274a8ffc018ddb08b48b5c0a55051b6fa3a
Brian Menendez <Brianclikclak@hotmail.com>
2023-09-14 15:16:11 -0700

    new components imported

--- gaming-grotto/src/main/frontend/src/views/HomeView.vue
+++ gaming-grotto/src/main/frontend/src/views/HomeView.vue
@@ -1,8 +1,29 @@
 <script setup>
-import NavBar from '../components/NavBar.vue';
-
+import NavBar from "../components/NavBar.vue";
+import TrendGame from "../components/TrendGame.vue";
+import InfoExtra from "../components/InfoExtra.vue";
+import TrendingGames from "../components/TrendingGames.vue"
 </script>
 
 <template>
-  <NavBar/>
+  <header>
+    <NavBar />
+  </header>
+  <main>
+    <div>
+      <TrendGame />
+    </div>
+    <TrendingGames/>
+    <div>
+      <InfoExtra/>
+    </div>
+  </main>
 </template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
