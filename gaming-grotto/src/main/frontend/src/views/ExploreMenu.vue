b0a690c1bf543c3508df5f80ea539ce81160fe9c
Brianclikclak <brianclikclak@hotmail.com>
2023-09-18 21:42:24 +0200

    new view for show all games

---
+++ gaming-grotto/src/main/frontend/src/views/ExploreMenu.vue
@@ -0,0 +1,116 @@
+<script setup>
+import { ref, onMounted, defineEmits } from "vue";
+import GameDataService from "../services/GameDataService";
+import { useRouter } from "vue-router";
+import NavBar from "../components/NavBar.vue";
+
+const games = ref([]);
+
+const router = useRouter();
+
+const  emit  = defineEmits();
+const showGameDetails = (game) => {
+  emit("show-details", game);
+
+  router.push({ name: "GameDetails", params: { id: game.id } });
+};
+
+onMounted(async () => {
+  try {
+    const response = await GameDataService.get("api/games");
+    games.value = response.data;
+    console.log(games.value);
+  } catch (error) {
+    console.error("Error al cargar los juegos:", error);
+  }
+});
+</script>
+
+<template>
+    <div>
+      <NavBar />
+      <div class="main-container">
+        <div class="container-header">
+          <h3 class="container-header__title">Juegos</h3>
+          <BtnShowAll />
+        </div>
+        <div class="genres-container">
+          <div
+            v-for="game in games"
+            :key="game.id"
+            class="genres-container__action"
+            @click="showGameDetails(game)"
+          >
+            <img class="image-game" :src="game.image" alt="" />
+            <p class="game-title">{{ game.title }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+
+<style scoped lang="scss">
+@use "../scss/colors" as c;
+
+.main-container {
+  background-color: map-get(c.$colors, "dark-gray");
+  display: flex;
+  flex-direction: column;
+  padding-top: 4.5em;
+}
+.container-header {
+  display: flex;
+  justify-content: space-around;
+  padding: 2em;
+  background-color: map-get(c.$colors, "dark-gray");
+  color: map-get(c.$colors, "white");
+  width: 74%;
+  align-self: center;
+
+  .container-header__title {
+    font-size: 30px;
+    margin-right: auto;
+  }
+}
+
+.genres-container {
+  background-color: map-get(c.$colors, "dark-gray");
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1em;
+  width: 70%;
+  margin: auto;
+  justify-content: space-between;
+
+  .game-title {
+    color: map-get(c.$colors, "white");
+    font-size: 25px;
+    padding-top: 0.5em;
+  }
+}
+
+.genres-container__action {
+  text-align: center;
+  border-radius: 10px;
+  flex: 0 calc(33.33% - 1em);
+  padding-bottom: 2em;
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.image-game {
+  width: 100%;
+  border-radius: 20px;
+}
+
+@media screen and (max-width: 768px) {
+  .genres-container__action {
+    flex: 0 calc(100% - 2em);
+    margin: auto;
+    flex-wrap: nowrap;
+  }
+}
+</style>
